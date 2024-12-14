6633786b037ffcd4990fbed0b0226a431e904656
Egor Marvelous <marvelchenko@gmail.com>
2024-12-14 09:21:21 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import './App.css';
+import { closestCorners, DndContext, KeyboardSensor, PointerSensor, TouchSensor, useSensor, useSensors } from '@dnd-kit/core';
+import Column from './components/Column';
+import { arrayMove, sortableKeyboardCoordinates } from '@dnd-kit/sortable';
+import Input from './components/Input/Input';
+
+function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, title: "Add tests to homepage"},
+    { id: 2, title: "Fix styling in about section"},
+    { id: 3, title: "Learn how to center a div"},
+  ]);
+
+  const addTask = (title) => {
+    setTasks((tasks) => [...tasks, { id: tasks.length + 1, title }]);
+  };
+  
+  const getTaskPos = id => tasks.findIndex((task) => task.id === id);
+
+  const handleDragEnd = event => {
+    const {active, over} = event;
+
+    if(active.id === over.id) 
+      return;
+
+    setTasks(tasks => {
+      const originalPos = getTaskPos(active.id);
+      const newPos = getTaskPos(over.id);
+
+      return arrayMove(tasks, originalPos, newPos);
+
+    });
+  };
+
+  const sensors = useSensors (
+    useSensor(PointerSensor),
+    useSensor(TouchSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  )
+  return (
+    <div className="App">
+      <h1>Hello, World</h1>
+      <DndContext
+      sensors={sensors} 
+      onDragEnd={handleDragEnd}
+      collisionDetection={closestCorners}>
+        <Input onSubmit={addTask} />
+        <Column tasks={tasks} />
+      </DndContext>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Input/Input.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import './Input.css'
+
+const Input = ({ onSubmit }) => {
+    const [input, setInput] = useState("")
+
+    const handleSubmit = () => {
+        if (!input) return;
+
+        onSubmit(input);
+
+        setInput("");
+    }
+  return (
+    <div className='container'>
+      <input type="text" className='input' value={input}
+      onChange={(e) => setInput(e.target.value)} />
+
+      <button onClick={handleSubmit} className='button'>
+        Add
+      </button>
+    </div>
+  )
+}
+
+export default Input
